6c7139b9bd8ec6a996385c636f85d3344a22c9e9
Iago Almeida <iagoalmeidadev@outlook.com>
2023-04-05 16:51:32 -0300

    Adicionando curso

--- CRUD-Angular/src/app/courses/course-form/course-form.component.ts
+++ CRUD-Angular/src/app/courses/course-form/course-form.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { CoursesService } from '../services/courses.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-course-form',
@@ -12,7 +13,10 @@ export class CourseFormComponent {
 
   form: FormGroup;
 
-  constructor( private formBuilder: FormBuilder, private service: CoursesService, private snackBar: MatSnackBar){
+  constructor( private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private snackBar: MatSnackBar,
+    private location: Location){
     this.form = this.formBuilder.group({
       name: [null],
       category:[null]
@@ -20,11 +24,16 @@ export class CourseFormComponent {
   }
 
   onSubmit(){
-    this.service.save(this.form.value).subscribe(result => console.log(result), error => this.onError());
+    this.service.save(this.form.value).subscribe(result => this.onSucess(), error => this.onError());
   }
 
   onCancel(){
-    console.log('onCancel');
+    this.location.back();
+  }
+
+  private onSucess(){
+    this.snackBar.open('Curso Salvo com sucesso!', '', {duration: 3000});
+    this.onCancel();
   }
 
   private onError(){
